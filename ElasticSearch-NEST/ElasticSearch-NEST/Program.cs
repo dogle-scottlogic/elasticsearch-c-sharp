01b25527fa8a4f4ac2e9fa5c57bd0f04b45dedb3
Dave Ogle <dogle@scottlogic.co.uk>
2018-02-07 15:06:49 +0000

    Create Solution

---
+++ ElasticSearch-NEST/ElasticSearch-NEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElasticSearch_NEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
